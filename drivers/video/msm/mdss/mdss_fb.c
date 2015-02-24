07f1fec1db2f70728c2a493a05be3d0d383aa959
Shivaraj Shetty <shivaraj@codeaurora.org>
2015-02-24 21:47:56 +0530

    msm: mdss: add support for configuring fb format
    With current implementation frame buffer format is
    fixed to RGBA8888. Add support to configure the same
    based on target through dtsi.
    
    Change-Id: I60de1d6c34f324c17ca83ed46743e29b9f17d71c
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -860,6 +860,7 @@ static int mdss_fb_probe(struct platform_device *pdev)
 	struct msm_fb_data_type *mfd = NULL;
 	struct mdss_panel_data *pdata;
 	struct fb_info *fbi;
+	const char *data;
 	int rc;
 	u32 cell_index = 0;
 
@@ -910,6 +911,20 @@ static int mdss_fb_probe(struct platform_device *pdev)
 		mfd->bl_level_scaled = mfd->bl_level;
 		if (!IS_CALIB_MODE_BL(mfd))
 			mdss_fb_scale_bl(mfd, &mfd->bl_level_scaled);
+	}
+
+	if (mfd->panel.type == MIPI_VIDEO_PANEL ||
+				mfd->panel.type == MIPI_CMD_PANEL) {
+		rc = of_property_read_string(pdev->dev.of_node,
+				"qcom,mdss-fb-format", &data);
+		if (!rc) {
+			if (!strcmp(data, "rgb888"))
+				mfd->fb_imgType = MDP_RGB_888;
+			else if (!strcmp(data, "rgb565"))
+				mfd->fb_imgType = MDP_RGB_565;
+			else
+				mfd->fb_imgType = MDP_RGBA_8888;
+		}
 	}
 
 	mfd->pdev = pdev;
